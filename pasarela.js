307122c85e0908d0e50848a46228524c49881e65
guidoxmartina <48781895+guidoxmartina@users.noreply.github.com>
2022-10-13 11:20:36 -0300

    con movimiento

--- pasarela.js
+++ pasarela.js
@@ -28,19 +28,59 @@ function Personaje(x,y,nombre) {
 var personajeUno=new Personaje(10,20,"Viejito");
 
 document.addEventListener('keydown',function(e){
+    //agregue esto
+    x=document.getElementById("personaje").offsetLeft;
+    y=document.getElementById("personaje").offsetTop;
+    document.getElementById("personaje").style.position="relative";
+
+
     //console.log(e); //muestra que tecla se presiona
     switch(e.key){
         case "ArrowLeft":
             personajeUno.izquierda();
+            document.getElementById("personaje").style.left=x-20+"px";
+
         break;
         case "ArrowUp":
             personajeUno.subir();
+            document.getElementById("personaje").style.top=y-30+"px";
+
         break;
         case "ArrowDown":
             personajeUno.bajar();
+            document.getElementById("personaje").style.top=y-30+"px";
+
         break;
         case "ArrowRight":
             personajeUno.derecha();
+            document.getElementById("personaje").style.left=x+10+"px";
         break;
     }
-})
+});
+
+
+/* 
+var posicionX=0
+var posicionY=0
+document.addEventListener("keydown", function(e){
+    posicionX=document.getElementById("personaje").offsetLeft;
+    posicionY=document.getElementById("personaje").offsetTop;
+    document.getElementById("personaje").style.position="relative";
+    switch(e.key){
+        case "j":
+            document.getElementById("personaje").style.left=posicionX-20+"px";
+        break;
+        case "i":
+            document.getElementById("personaje").style.top=posicionY-30+"px";
+        break;
+        case "k":
+            document.getElementById("personaje").style.top=posicionY+15+"px";
+        break;
+        case "l":
+            document.getElementById("personaje").style.left=posicionX+10+"px";
+        break;
+    }
+});
+
+
+*/
