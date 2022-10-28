7d372416a7a3ec24ccd16a26a1187b6d6adb5022
guidoxmartina <48781895+guidoxmartina@users.noreply.github.com>
2022-10-28 17:55:38 -0300

    version nicol

--- pasarela.js
+++ pasarela.js
@@ -1,30 +1,36 @@
 //variables del canvas
 var canvas;
 var ctx;
-//variables para el juego
 
+//variables para el juego
 var puntos=0;
 var vidas=3;
 
 //variables para las imagenes
 var imgPersonajeUno=new Image();
 var imgAplauso=new Image();
-var imgBondi=new Image();
+var imgBondiUno=new Image();
+var imgBondiDos=new Image();
 
 //variables fondo
 var posicionFondo=0;
 
+/*variables para los audios*/
+var audioPerdida;
+var audioPuntos;
+
 //crear objetos
-var personajeUno= new Personaje(50,200,74,220);
-var aplausoUno= new Elemento(100,100,65,59,'aplauso');
-var bondiUno= new Elemento(200,200,225,200,'bondi');
+var personajeUno= new Personaje(50,200,40,90);
+var aplauso= new Elemento(850,150,65,59,'aplauso',imgAplauso);
+var bondiUno= new Elemento(850,200,90,70,'bondiUno',imgBondiUno);
+var bondiDos= new Elemento(1300,300,90,70,'bondiDos',imgBondiDos);
+
 
 function dibujar() {
     //selecciono canvas
     canvas=document.getElementById("canvas");
     //asigno imagen de fondo
-    canvas.style.backgroundImage="url(img/fondosinarboles.png)";
-   
+    canvas.style.backgroundImage="url(img/fondo.png)";
     //establezco contexto
     ctx=canvas.getContext("2d");
     dibujaTexto();
@@ -32,29 +38,73 @@ function dibujar() {
     //conecto la imagen
     imgPersonajeUno.src='img/personaje.png';
     //ubicacion
-    imgPersonajeUno.onload=function () {
-       // ctx.drawImage(imgPersonajeUno,250,150,74,220);//puede recibir 3 o 5(nombre objeto,posX,posY,ancho,alto)
-        personajeUno.dibuja();
+    imgPersonajeUno.onload=function(){
+    personajeUno.dibuja();
     }
     //dibujar Aplauso
     imgAplauso.src="img/aplauso.png";
     imgAplauso.onload=function(){
-        aplausoUno.dibujaElemento(imgAplauso);
+        aplauso.dibujaElemento();
+    }
+    //dibujar Bondi uno
+    imgBondiUno.src="img/bondi.png";
+    imgBondiUno.onload=function(){
+        bondiUno.dibujaElemento();
+    }
+    //dibujar Bondi dos
+    imgBondiDos.src="img/bondi.png";
+    imgBondiDos.onload=function(){
+        bondiDos.dibujaElemento();
     }
 
-    //dibujar Bondi
-    imgBondi.src="img/bondi.png";
-    imgBondi.onload=function(){
-        bondiUno.dibujaElemento(imgBondi);
-    }
-
+    //audios
+    audioPerdida=new Audio();
+    audioPerdida.src="audios/perdida.mp3";
+    audioPuntos=new Audio();
+    audioPuntos.src="audios/puntos.mp3";
 
 
     setInterval(function(){
         posicionFondo-=5;
         canvas.style.backgroundPosition=posicionFondo+"px 0px"
+        if(vidas>0 && puntos<100){
+            /*hacerlos mover*/
+            bondiUno.mover();
+            bondiDos.mover();
+            aplauso.mover();
+
+            /*chequear si colisionan*/
+            bondiUno.colision();
+            bondiDos.colision();
+            aplauso.colision();
+
+            ctx.clearRect(0,0,canvas.width,canvas.height);//borra
+            personajeUno.dibuja();
+            bondiUno.dibujaElemento();
+            bondiDos.dibujaElemento();
+            aplauso.dibujaElemento();
+            dibujaTexto();
+        }else if (vidas<=0){
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            ctx.font="80px Impact";
+            ctx.fillText("PERDISTE",250,330);
+        }else if (puntos=100){
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            ctx.font="80px Impact";
+            ctx.fillText("GANASTE",250,330);
+        }
     },1000/25);
+
+    
 }
+
+function dibujaTexto(){
+    /*definir un tamaño y tipo*/
+    ctx.font="20px Impact";
+    ctx.fillText("Puntos: "+puntos,20,40);
+    ctx.fillText("Vidas: "+vidas,715,40);
+}
+
 //personaje
 function Personaje(x,y,ancho,alto){
     //atributos
@@ -68,16 +118,15 @@ function Personaje(x,y,ancho,alto){
         this.x+=10;
         posicionFondo--;
     }
-    this.izquierda=function(){
-        this.x-=10;
-        posicionFondo++;
-
-    }
     this.arriba=function(){
-        this.y-=10;
+    if(this.y>115){
+        this.y-=30;
+    }
     }
     this.abajo=function(){
-        this.y+=10;
+        if(this.y<400){
+            this.y+=30;
+        }
     }
     this.dibuja=function(){
         ctx.drawImage(imgPersonajeUno,this.x,this.y,this.ancho,this.alto);
@@ -88,63 +137,62 @@ function Personaje(x,y,ancho,alto){
 
 
 //objetos
-function Elemento(x,y,ancho,alto,tipo){
+function Elemento(x,y,ancho,alto,tipo,imagen){
     this.x=x;
     this.y=y;
     this.ancho=ancho;
     this.alto=alto;
     this.tipo=tipo;
+    this.imagen=imagen;
     //metodos
 
-    this.dibujaElemento=function(img){
-        ctx.drawImage(img,this.x,this.y,this.ancho,this.alto);
+    this.dibujaElemento=function(){
+        ctx.drawImage(this.imagen,this.x,this.y,this.ancho,this.alto);
     }
-    
-}
+    this.mover=function(){
+        if(this.x>-50){
+            this.x-=10;
+        }else{
+            this.sortear();
+        }
+    }
+    this.sortear=function(){
+        /*
+            Math.floor(
+                Math.random() * (max - min + 1)
+            )+ min;
+        */
+        //rango para x máximo sea 1400, min 850
+        this.x=Math.floor(Math.random()*(1400 - 850 +1))+850;
 
-
-
-
-//dibujar texto del juego
-function dibujaTexto(){   
-/*dibujar rectangulo
-//puntos
-ctx.fillStyle="black";
-ctx.fillRect(20,28,80,30);//x,y,ancho,alto
-*/
-//dibujar texto
-    //puntos
-    ctx.fillStyle="black";
-    ctx.font="20px Impact";
-    //puntos
-    ctx.fillText('Puntos: '+puntos,20,40)
-    //vidas
-    ctx.fillText('Vidas: '+vidas,715,40)
+        //rango para y máximo sea 400, minimo 172
+        this.y=Math.floor(Math.random()*((400)-(172)+1))+(172);
+    }
+    this.colision=function(){
+        if(
+            (this.x>personajeUno.x-this.ancho)
+            &&(this.x<personajeUno.x+personajeUno.ancho)
+            &&(this.y>personajeUno.y-this.alto)
+            &&(this.y<personajeUno.y+personajeUno.alto)
+        ){
+            this.sortear();
+            if(this.tipo=="aplauso"){
+                puntos+=10;
+                audioPuntos.play();
+            }else{
+                vidas--;
+                audioPerdida.play();
+            }
+        }
+    }
 }
 
 //escuchador
 document.addEventListener('keydown',function(e){
     
     switch(e.key){
-        //movimiento WASD
-        case "w":
-            personajeUno.arriba();
         
-        break;
-        case "a":
-            personajeUno.izquierda();
-        break;
-        case "s":
-            personajeUno.abajo();
-        break;
-        case "d":
-            personajeUno.derecha();
-        break;
         //movimiento flechas
-        case "ArrowLeft":
-            personajeUno.izquierda();
-
-        break;
         case "ArrowUp":
             personajeUno.arriba();
 
@@ -157,13 +205,8 @@ document.addEventListener('keydown',function(e){
             personajeUno.derecha();
         break;
     }
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    dibujaTexto();
-    
-    bondiUno.dibujaElemento(imgBondi);
-    aplausoUno.dibujaElemento(imgAplauso);
-    personajeUno.dibuja();
-
-
+    if(vidas>0){
+        canvas.style.backgroundPosition=posicionFondo+"px 0px";
+    }
 });
 
